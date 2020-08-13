33bbe5dd3778ba1a8e468633b47d771fd25a35db
Mutiara31 <68982126+Mutiara31@users.noreply.github.com>
2020-08-13 22:01:00 +0700

    Add files via upload

---
+++ Expressions.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>){
+    val nilai = 75
+    if(nilai >= 75){
+        println("Nilai_kamu " +nilai+ ", selamat Ya!")
+    }else{
+        println("Nilai_kamu " +nilai+ ", silahkan_ikuti_remidial.")
+    }
+}
---
+++ Hello Kotlin.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val nama = "Mutiara Indah Hapsari"
+    print("Nama saya ")
+    println(nama)
+    val Hobby = "Masak"
+    print("Hobby saya ")
+    println(Hobby)
+}
